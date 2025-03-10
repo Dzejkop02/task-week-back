97e5801eb9454f9739186debb1b16f82e92c1098
JakubWiecek <jakub.wiecek.skrzynka@gmail.com>
2025-03-10 18:32:55 +0100

    Extended server configuration.

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    // allowedHeaders: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+}));
+app.use(cookieParser());
+
+app.use(express.urlencoded({
+    extended: true,
+}));
 app.use(express.json());
 
 
